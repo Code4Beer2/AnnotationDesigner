5c3bb15bf32f05fc9b80888a2397a11e201820b0
Code4Beer2 <veilleux.laurent@gmail.com>
2017-07-07 00:09:18 +0200

    better steup scripts + pyInstaller + drag and drop items
    better steup scripts + pyInstaller + drag and drop items

---
+++ setupPython/addPythonDirToEnvPath.ps1
@@ -0,0 +1,33 @@
+function Add-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -notcontains $Path) {
+            $persistedPaths = $persistedPaths + $Path | where { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -notcontains $Path) {
+        $envPaths = $envPaths + $Path | where { $_ }
+        $env:Path = $envPaths -join ';'
+    }
+}
+
+Write-Host "adding c:\Python27 to User PATH env"
+Add-EnvPath "c:\Python27" "User"
+Write-Host "c:\Python27 added to User PATH env"
